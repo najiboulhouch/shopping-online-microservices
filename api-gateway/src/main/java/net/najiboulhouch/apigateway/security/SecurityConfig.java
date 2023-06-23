24427a955e79b04145fc79057c59d26dd56aa8c3
Najib OULHOUCH <developpersgroups@gmail.com>
2023-06-23 08:00:35 +0200

    Adding Api-Gateway microservice to repository.

---
+++ api-gateway/src/main/java/net/najiboulhouch/apigateway/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package net.najiboulhouch.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**" , "**/prometheus")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        return serverHttpSecurity.build();
+    }
+}
